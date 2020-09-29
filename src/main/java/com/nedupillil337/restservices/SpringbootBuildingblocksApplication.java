595985321cb43813e683ec2a00e624d56019d25a
nedupillil337 <nedupillil337@gmail.com>
2020-09-29 17:38:36 +0530

    second commit

--- src/main/java/com/nedupillil337/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/nedupillil337/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.nedupillil337.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+//sample
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
